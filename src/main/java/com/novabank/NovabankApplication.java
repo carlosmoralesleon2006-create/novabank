3ea3fa01c7440a900f906c571d56caa78189be50
CharlieDBest <carlosmoralesleon2006@gmail.com>
2026-04-08 11:34:39 +0200

    Pasamos el proyecto a SpringBoot

---
+++ src/main/java/com/novabank/NovabankApplication.java
@@ -0,0 +1,13 @@
+package com.novabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovabankApplication.class, args);
+	}
+
+}
